2e2ee20659598d9fab9a513f686f17a8974a9fa6
atilagulers <atilagulerofficial@gmail.com>
2023-10-05 02:14:35 +0300

    .

--- app.js
+++ app.js
@@ -46,8 +46,8 @@ const port = process.env.PORT || 3000;
 
 const startServer = async () => {
   try {
-    await connectDB();
-    updatePharmacies();
+    //await connectDB();
+    //updatePharmacies();
     app.listen(port, () => {
       console.log(`Server is listening on port ${port}`);
     });
