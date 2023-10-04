8926a98b393e30bff269fbae4afe3b1e224351b4
atilagulers <atilagulerofficial@gmail.com>
2023-10-05 00:48:18 +0300

    .

--- app.js
+++ app.js
@@ -34,23 +34,63 @@ app.get('/api/v1', (req, res) => {
 });
 
 // serving frontend
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-path.join(__dirname, 'client/build');
-app.get('*', (req, res) => {
-  const indexPath = path.join(__dirname, 'client', 'build', 'index.html');
-  res.sendFile(indexPath, (err) => {
-    if (err) {
-      res
-        .status(500)
-        .send({
-          err,
-          path1: `${path.join(__dirname, 'client/build')}`,
-          path2: `${path.join(__dirname, 'client', 'build', 'index.html')}`,
-        });
-    }
-  });
+// #############################################################################
+// Logs all request paths and method
+app.use(function (req, res, next) {
+  res.set('x-timestamp', Date.now());
+  res.set('x-powered-by', 'cyclic.sh');
+  console.log(
+    `[${new Date().toISOString()}] ${req.ip} ${req.method} ${req.path}`
+  );
+  next();
 });
+
+// #############################################################################
+// This configures static hosting for files in /public that have the extensions
+// listed in the array.
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html', 'css', 'js', 'ico', 'jpg', 'jpeg', 'png', 'svg'],
+  index: ['index.html'],
+  maxAge: '1m',
+  redirect: false,
+};
+app.use(express.static('public', options));
+
+// #############################################################################
+// Catch all handler for all other request.
+app.use('*', (req, res) => {
+  res
+    .json({
+      at: new Date().toISOString(),
+      method: req.method,
+      hostname: req.hostname,
+      ip: req.ip,
+      query: req.query,
+      headers: req.headers,
+      cookies: req.cookies,
+      params: req.params,
+    })
+    .end();
+});
+//app.use(express.static(path.join(__dirname, 'client/build')));
+
+//path.join(__dirname, 'client/build');
+//app.get('*', (req, res) => {
+//  const indexPath = path.join(__dirname, 'client', 'build', 'index.html');
+//  res.sendFile(indexPath, (err) => {
+//    if (err) {
+//      res
+//        .status(500)
+//        .send({
+//          err,
+//          path1: `${path.join(__dirname, 'client/build')}`,
+//          path2: `${path.join(__dirname, 'client', 'build', 'index.html')}`,
+//        });
+//    }
+//  });
+//});
 
 app.use(errorHandlerMiddleware);
 app.use(notFoundMiddleware);
