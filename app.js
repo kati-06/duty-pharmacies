a3c90127ae4008c8dfba7c552fc257024babc010
atilagulers <atilagulerofficial@gmail.com>
2023-10-05 00:33:16 +0300

    .

--- app.js
+++ app.js
@@ -34,11 +34,11 @@ app.get('/api/v1', (req, res) => {
 });
 
 // serving frontend
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 
 app.get('*', (req, res) => {
   res.sendFile(
-    path.join(__dirname, 'client', 'build', 'index.html'),
+    path.join(__dirname, './client/build/index.html'),
     function (err) {
       res.status(500).send(err);
     }
